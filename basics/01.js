07647164b7e10f11941ec6eb4399f3ea8fcbd52c
Gulshan Kumar <103372647+Gulshankumar22@users.noreply.github.com>
2024-07-15 19:51:59 +0000

    operations

--- basics/01.js
+++ basics/01.js
@@ -11,4 +11,14 @@ let change=Number(score)
 // let check=1; it gives true because 1 => true, 0 => false
 let check="sahil" //its also gives true but when it is empty is gives false
 let booleanconvert= Boolean(check)
-console.log(booleanconvert)
+// console.log(booleanconvert)
+
+/****************************************operations****************************** */
+
+let value=3;
+let negvalue=-value; //here we are assigning the negative value
+console.log(negvalue);
+console.log("1"+2); //output is 12
+console.log(1+"2"); //output is 12
+console.log("1"+2+2);//output is 122 
+console.log(1+2+"2");// output is 32

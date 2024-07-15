f3b01c7bb9b616f81f52c2126ea2f6db38c1b8a7
Gulshan Kumar <103372647+Gulshankumar22@users.noreply.github.com>
2024-07-15 19:16:28 +0000

    conversion of datatype

--- basics/01.js
+++ basics/01.js
@@ -1,2 +1,14 @@
-console.log("gulshan")
-console.log("kumar")
+let score="3ark"
+// console.log(typeof score )
+let change=Number(score)
+// console.log(typeof change )
+// console.log(change) //here it give us NaN 
+
+
+//"33" => 33
+//"33ABC" => NaN
+
+// let check=1; it gives true because 1 => true, 0 => false
+let check="sahil" //its also gives true but when it is empty is gives false
+let booleanconvert= Boolean(check)
+console.log(booleanconvert)
